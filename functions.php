62072cf57d7810f170ad05bb5607d567f7901bef
Vasile <allfortrashmail@gmail.com>
2021-02-14 12:59:50 +0200

    added functions.php

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Blocksy Child theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Blocksy Child
+ */
+
+define( "CURRENT_THEME_PATH", get_stylesheet_directory_uri() );
+
+// Includes
+require_once 'inc/setup.php';
+require_once 'inc/enqueue.php';
